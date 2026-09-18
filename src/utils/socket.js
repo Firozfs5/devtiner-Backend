623637ba5c084ea279c4048fef28d5434df5ed2a
Firoz <146817644+Firozfs5@users.noreply.github.com>
2026-09-19 00:13:53 +0530

    signalling is fixed

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -4,7 +4,7 @@ const Chat = require("../models/chat");
 const initializeSocket = (server) => {
   const io = socket(server, {
     cors: {
-      origin: "http://localhost:5173",
+      origin: process.env.FRONTEND_URL,
       credentials: true,
     },
   });
